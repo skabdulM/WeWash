005c257d29697c68625913b5873a636f4f02f198
Shaikh Abdul Mannan <abdulmannan8334@gmail.com>
2025-04-12 00:18:57 +0530

    feat: update Schedule Pickup & Services pages
    - Schedule A Pickup: Removed steps 2, 3, and 4
    - Schedule A Pickup: Added optional 'address' field to Step 1
    - Services & Pricing: Removed 'Select a location' dropdown
    - Services & Pricing: Removed 'Moth/Mite Treatment' category

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,6 +1,3 @@
 function redirect_to_service(service_type) {
-    const selectElement = document.getElementById('service_location');
-    const selectedLocation = selectElement.value;
-
-    window.location.href = `./services_and_pricing.html?service=${service_type}&location=${selectedLocation}`;
+    window.location.href = `./services_and_pricing.html?service=${service_type}`;
 }
